630b8da2eb3f2761479ae13b0ece8af38819db4a
yasserfathy111 <45918922+yasserfathy111@users.noreply.github.com>
2019-02-24 15:39:04 +0200

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,2 +1,3 @@
-cins --help
-ctc --help
+ctc -E main.c -o main.i
+
+#cins main.i -c .\rvs_instr.h -c .\rvs_ipoint.h --exf .\tmp\xsc\exf.exf --force-hline -o .\tmp\instr\%%f.c --xsc .\tmp\xsc\%%f.xsc --cext ./TASKING-custom.cext --nbits 31
