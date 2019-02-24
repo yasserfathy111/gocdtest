77437717b4440025a818a0169581c9e29c52b55c
yasserfathy111 <45918922+yasserfathy111@users.noreply.github.com>
2019-02-24 20:20:54 +0200

    Update Instrument.bat

--- Instrument.bat
+++ Instrument.bat
@@ -1,1 +1,1 @@
-	cins rvs/preprocessed/main.i -c ./rvs/lib/rvs_instr.h -c ./rvs/lib/rvs_ipoint.h --exf ./rvs/xsc/exf.exf --force-hline -o ./rvs/instrumented/main.c --xsc ./rvs/xsc/main.xsc --cext ./TASKING-custom.cext
+	cins rvs/preprocessed/main.i -c ./rvs/lib/rvs_instr.h -c ./rvs/lib/rvs_ipoint.h --exf ./rvs/xsc/exf.exf --force-hline -o ./rvs/instrumented/main.c --xsc ./rvs/xsc/main.xsc
